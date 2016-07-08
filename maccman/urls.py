64003469d5c9df2f0e8ed4dcb4bc3d8fc43b1677
Felix Morgner <felix.morgner@gmail.com>
2016-07-08 10:55:12 +0200

    Move logout view to class based

--- maccman/urls.py
+++ maccman/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.core.urlresolvers import reverse
 
 from . import views
 
@@ -7,13 +8,17 @@ app_name = 'maccman'
 urlpatterns = [
     url(r'^$',
         views.IndexView.as_view(),
-        name='index'),
+        name='index',),
 
-    url(r'^login/$',
+    url(r'^login$',
         views.LoginView.as_view(),
-        name='login'),
+        name='login',),
 
-    url(r'^logout/$',
-        views.logout_view,
-        name='logout'),
+    url(r'^logout$',
+        views.LogoutView.as_view(),
+        name='logout',),
+
+    url(r'^domains$',
+        views.UserDomainsListView.as_view(),
+        name='domains',),
 ]
--- maccman/views.py
+++ maccman/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import logout, login
-from django.contrib.auth.forms import AuthenticationForm
-from django.core.urlresolvers import reverse
-from django.shortcuts import redirect
-from django.views.generic.base import TemplateView
+from django.core.urlresolvers import reverse_lazy
+
+from django.views.generic.base import TemplateView, RedirectView
 from django.views.generic.edit import FormView
+from .models import Domain, Mailbox, UserProfile
+from .forms import UserProfileAuthenticationForm
 
 
 class IndexView(TemplateView):
@@ -15,17 +16,33 @@ class IndexView(TemplateView):
 
 
 class LoginView(FormView):
+
     template_name = 'maccman/login.html'
-    form_class = AuthenticationForm
+    form_class = UserProfileAuthenticationForm
+    index = reverse_lazy('maccman:index')
+
+    def get_context_data(self, **kwargs):
+        context = super(LoginView, self).get_context_data(**kwargs)
+        context['next'] = self.request.GET.get('next', self.index)
+        return context
+
+    def get_success_url(self):
+        return self.request.POST.get('next', self.index)
 
     def form_valid(self, form):
         login(self.request, form.get_user())
         return super(LoginView, self).form_valid(form)
 
-    def get_success_url(self):
-        return reverse('maccman:index')
+
+class LogoutView(RedirectView):
+
+    permanent = False
+    query_string = False
+    pattern_name = 'maccman:index'
+
+    def get_redirect_url(self, *args, **kwargs):
+        logout(self.request)
+        return super(LogoutView, self).get_redirect_url(*args, **kwargs)
 
 
-def logout_view(request):
-    logout(request)
-    return redirect('maccman:index')
+
